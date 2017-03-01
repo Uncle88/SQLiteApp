3e410037ab3d95844e331938c07a4888984c5615
Uncle88 <andreyproskura88@gmail.com>
2017-03-02 00:24:30 +0200

    Created Model for our DB table

---
+++ SQLiteApp/SQLiteApp/Model/Market.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite.Net.Attributes;
+
+namespace SQLiteApp.Model
+{
+    public class Market
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Price { get; set; }
+    }
+}
